fca9e28ab23005b5a1b7e8fa935fc674cf816ad9
2190303755 <2190303755@qq.com>
2025-05-24 17:45:21 +0800

    fix dragon armor recipes

--- data/DragonArmorMaterial.py
+++ data/DragonArmorMaterial.py
@@ -1,23 +1,23 @@
 from enum import Enum
-from Core.Util import makeId, TagKey, cast2Stack
+from Core.Util import makeId, TagKey
 class DragonArmorMaterial(Enum):
   IRON = [
     makeId("iron_dragon_armor"),
     TagKey('ingotIron'),
-    cast2Stack('iron_block')
+    TagKey('blockIron')
   ]
   GOLD = [
     makeId("golden_dragon_armor"),
-    TagKey('gold_ingot'),
-    cast2Stack('gold_block')
+    TagKey('ingotGold'),
+    TagKey('blockGold')
   ]
   EMERALD = [
     makeId("emerald_dragon_armor"),
-    TagKey('ingotGold'),
-    cast2Stack('emerald_block')
+    TagKey('gemEmerald'),
+    TagKey('blockEmerald')
   ]
   DIAMOND = [
     makeId("diamond_dragon_armor"),
     TagKey('gemDiamond'),
-    cast2Stack('diamond_block')
+    TagKey('blockDiamond')
   ]
